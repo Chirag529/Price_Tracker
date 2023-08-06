8d72ca76af7521365ed5c457b082dd9bfad74ad5
Chirag Tiwari <87868888+Chirag529@users.noreply.github.com>
2023-08-06 22:43:27 +0530

    Update tracker.py
    Add URL link to mail with Cost of Device

--- tracker.py
+++ tracker.py
@@ -11,9 +11,10 @@ load_dotenv()
 
 urllib3.disable_warnings()
 
+URL = 'https://www.flipkart.com/apple-iphone-13-starlight-128-gb/p/itmc9604f122ae7f?pid=MOBG6VF5ADKHKXFX&lid=LSTMOBG6VF5ADKHKXFXQGX7PK&marketplace=FLIPKART&q=iphone&store=tyy%2F4io&spotlightTagId=BestsellerId_tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=821753ca-4b19-4889-bb79-c3c5f197b93a.MOBG6VF5ADKHKXFX.SEARCH&ppt=sp&ppn=sp&ssid=ioo97iq8280000001691306394953&qH=0b3f45b266a97d70'
 # Using a While loop to make sure that our code runs all the time (once a day)
 while True:
-    re = requests.get('https://www.flipkart.com/apple-iphone-13-starlight-128-gb/p/itmc9604f122ae7f?pid=MOBG6VF5ADKHKXFX&lid=LSTMOBG6VF5ADKHKXFXQGX7PK&marketplace=FLIPKART&q=iphone&store=tyy%2F4io&spotlightTagId=BestsellerId_tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=821753ca-4b19-4889-bb79-c3c5f197b93a.MOBG6VF5ADKHKXFX.SEARCH&ppt=sp&ppn=sp&ssid=ioo97iq8280000001691306394953&qH=0b3f45b266a97d70', verify=False)
+    re = requests.get(URL, verify=False)
     # print(re)
 
     res = re.content
@@ -40,7 +41,7 @@ while True:
 
         recipients = ['Recept_name1','Recept_name2', ...]
         smt.sendmail(email, recipients,
-                    f"Subject: Headphones Price Notifier\n\nHi, price has dropped to {price}. Go buy it now!")
+                    f"Subject: Headphones Price Notifier\n\n Hi, the price has dropped to {price}!. Go buy it now! \n\ n LINK:"+ URL)
 
         smt.quit()
 
