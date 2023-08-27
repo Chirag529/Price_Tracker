7de3db6753b3b68bf7659c627073968c2abe2d7a
Chirag Tiwari <87868888+Chirag529@users.noreply.github.com>
2023-08-27 12:47:27 +0000

    PEP-8 guidelines

--- tracker.py
+++ tracker.py
@@ -9,40 +9,48 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# Disable SSL/TLS warnings
 urllib3.disable_warnings()
 
+# URL of the product page
 URL = 'https://www.flipkart.com/apple-iphone-13-starlight-128-gb/p/itmc9604f122ae7f?pid=MOBG6VF5ADKHKXFX&lid=LSTMOBG6VF5ADKHKXFXQGX7PK&marketplace=FLIPKART&q=iphone&store=tyy%2F4io&spotlightTagId=BestsellerId_tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=821753ca-4b19-4889-bb79-c3c5f197b93a.MOBG6VF5ADKHKXFX.SEARCH&ppt=sp&ppn=sp&ssid=ioo97iq8280000001691306394953&qH=0b3f45b266a97d70'
-# Using a While loop to make sure that our code runs all the time (once a day)
+
+# Using a While loop to run the code repeatedly (once a day)
 while True:
-    re = requests.get(URL, verify=False)
-    # print(re)
+    # Fetch the product page
+    response = requests.get(URL, verify=False)
+    res = response.content
 
-    res = re.content
-
+    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(res, 'html.parser')
-    # print(soup.prettify)
-    price_str = soup.find('div', attrs={'class': '_30jeq3 _16Jk6d'}).text[1:]  # type: ignore
-    # Remove the comma from the price text as it is not recognized as a valid float representation.
-    price_str = price_str.replace(',', '')
-    # Convert the modified price string to a float
-    price = float(price_str)
 
-    # Checking if the price is less than 40
-    # Use your email and password from environment variables
+    # Find and extract the price of the product
+    price_str = soup.find('div', attrs={'class': '_30jeq3 _16Jk6d'}).text[1:]
+    # Remove commas and convert the price to a float
+    price = float(price_str.replace(',', ''))
+
+    # Check if the price is below the threshold for notification
     if price < 60000:
         email = os.getenv("EMAIL")
         password = os.getenv("PASSWORD")
 
-        smt = smtplib.SMTP('smtp.gmail.com', 587)
-        smt.ehlo()
-        smt.starttls()
-        # Sender's credentials
-        smt.login(email, password)
+        # Connect to SMTP server
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(email, password)
 
-        recipients = ['Recept_name1','Recept_name2', ...]
-        smt.sendmail(email, recipients,
-                    f"Subject: Headphones Price Notifier\n\n Hi, the price has dropped to {price}!. Go buy it now! \n\ n LINK:"+ URL)
+        # List of recipients for the notification email
+        recipients = ['Recept_name1', 'Recept_name2', ...]
 
-        smt.quit()
+        # Compose the email subject and message
+        subject = "Headphones Price Notifier"
+        message = f"Hi, the price has dropped to {price}! Go buy it now!\n\nLINK: {URL}"
+        full_message = f"Subject: {subject}\n\n{message}"
 
-    time.sleep(24*60*60)  # Wait for 24 hours before checking the price again
+        # Send the notification email to recipients
+        smtp.sendmail(email, recipients, full_message)
+        smtp.quit()
+
+    # Wait for 24 hours before checking the price again
+    time.sleep(24 * 60 * 60)
